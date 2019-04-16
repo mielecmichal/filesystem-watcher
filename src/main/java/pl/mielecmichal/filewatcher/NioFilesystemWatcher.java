999c393215b0b93d3440b46d3a3e3a8bbc7f39c8
Michał Mielec <mielecmichal@gmail.com>
2019-04-17 01:49:32 +0200

    Add first basic tests

--- src/main/java/pl/mielecmichal/filewatcher/NioFilesystemWatcher.java
+++ src/main/java/pl/mielecmichal/filewatcher/NioFilesystemWatcher.java
@@ -3,14 +3,12 @@ package pl.mielecmichal.filewatcher;
 import io.vavr.control.Try;
 import lombok.Builder;
 import lombok.Value;
+import lombok.experimental.NonFinal;
 import lombok.extern.java.Log;
 
 import java.nio.file.*;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.function.Consumer;
 
 import static java.nio.file.StandardWatchEventKinds.*;
@@ -31,44 +29,56 @@ public class NioFilesystemWatcher implements FilesystemWatcher{
     private final ExecutorService consumersExecutor = Executors.newSingleThreadExecutor();
 
     private WatchService watcher = Try.of(() -> FileSystems.getDefault().newWatchService()).getOrElseThrow(IllegalStateException::new);
-
+    @NonFinal
+	private Future<?> consumer;
+	@NonFinal
+	private Future<?> producer;
     @Override
     public void watch() {
         Try.of(() -> watchedPath.register(watcher, ALL_WATCH_KINDS)).getOrElseThrow(IllegalStateException::new);
-        consumersExecutor.submit(this::consumeEvents);
-        producersExecutor.submit(this::produceEvents);
-    }
+		 consumer = consumersExecutor.submit(this::consumeEvents);
+		 producer = producersExecutor.submit(this::produceEvents);
+	}
 
     private void produceEvents() {
         try {
             WatchKey take = watcher.take();
             List<WatchEvent<?>> watchEvents = take.pollEvents();
             for (WatchEvent<?> watchEvent : watchEvents) {
-
+                log.fine(String.format("Found watch event = %s", watchEvent));
                 if (watchEvent.kind() == OVERFLOW) {
                     log.severe(String.format("OVERFLOW watchEvent occurred %s times. Operating system queue was overflowed. File events could be lost.", watchEvent.count()));
                     continue;
                 }
 
-                Path path = (Path) watchEvent.context();
+                Path filePath = (Path) watchEvent.context();
+				Path path = Paths.get(watchedPath.toString(), filePath.toString());
 
-                FilesystemEvent filesystemEvent = FilesystemEvent.builder()
+				FilesystemEvent filesystemEvent = FilesystemEvent.builder()
                         .eventType(FilesystemEvent.FilesystemEventType.of(watchEvent.kind()))
                         .path(path)
                         .build();
 
+
                 if (watchedConstraints.test(filesystemEvent)) {
-                    blockingQueue.put(filesystemEvent);
+					log.info("Found " + filesystemEvent);
+
+					blockingQueue.put(filesystemEvent);
                 }
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-        }
+        } catch (Exception e){
+        	log.severe(e.getMessage());
+		}
     }
 
     private void consumeEvents() {
         try {
-            watchedConsumer.accept(blockingQueue.take());
+            FilesystemEvent event = blockingQueue.take();
+			log.info("Took " + event);
+
+			watchedConsumer.accept(event);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
