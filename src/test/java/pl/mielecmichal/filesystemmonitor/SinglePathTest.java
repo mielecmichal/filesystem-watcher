4d004e5755edba36c6e9562fa35291b780a61b37
Michał Mielec <michal.mielec@zooplus.com>
2019-05-21 18:07:00 +0200

    Simplify reader

--- src/test/java/pl/mielecmichal/filesystemmonitor/SinglePathTest.java
+++ src/test/java/pl/mielecmichal/filesystemmonitor/SinglePathTest.java
@@ -25,111 +25,114 @@ import static pl.mielecmichal.filesystemmonitor.FilesystemEvent.FilesystemEventT
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SinglePathTest {
 
-	@TempDir
-	Path temporaryFolder;
+    @TempDir
+    Path temporaryFolder;
 
-	@Test
-	void shouldReadInitialFile() {
-		//given
-		Path testFile = Filesystem.createFile(temporaryFolder, "test.txt");
-		List<FilesystemEvent> receivedEvents = new ArrayList<>();
-		FilesystemMonitor monitor = FilesystemMonitor.builder()
-				.watchedPath(temporaryFolder)
-				.watchedConsumer(receivedEvents::add)
-				.build();
+    @Test
+    void shouldReadInitialFile() {
+        //given
+        Path testFile = Filesystem.createFile(temporaryFolder, "test.txt");
+        List<FilesystemEvent> receivedEvents = new ArrayList<>();
+        FilesystemMonitor monitor = FilesystemMonitor.builder()
+                .watchedPath(temporaryFolder)
+                .watchedConsumer(receivedEvents::add)
+                .build();
 
-		//when
-		monitor.startWatching();
+        //when
+        monitor.startWatching();
 
-		//then
-		Assertions.assertThat(receivedEvents).hasSize(1);
-		Assertions.assertThat(receivedEvents).extracting(FilesystemEvent::getPath).containsExactly(testFile);
-	}
+        //then
+        Assertions.assertThat(receivedEvents).hasSize(2);
+        Assertions.assertThat(receivedEvents).containsExactly(
+                FilesystemEvent.of(INITIAL, testFile),
+                FilesystemEvent.of(INITIAL, temporaryFolder)
+        );
+    }
 
-	@Test
-	void shouldWatchCreatedFile() throws InterruptedException {
-		//given
-		CountDownLatch countDownLatch = new CountDownLatch(1);
-		List<FilesystemEvent> receivedEvents = new ArrayList<>();
-		FilesystemMonitor monitor = FilesystemMonitor.builder()
-				.watchedPath(temporaryFolder)
-				.watchedConsumer(event -> {
-					receivedEvents.add(event);
-					countDownLatch.countDown();
-				})
-				.build();
+    @Test
+    void shouldWatchCreatedFile() throws InterruptedException {
+        //given
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        List<FilesystemEvent> receivedEvents = new ArrayList<>();
+        FilesystemMonitor monitor = FilesystemMonitor.builder()
+                .watchedPath(temporaryFolder)
+                .watchedConsumer(event -> {
+                    receivedEvents.add(event);
+                    countDownLatch.countDown();
+                })
+                .build();
 
-		//when
-		monitor.startWatching();
-		Path testFile = Filesystem.createFile(temporaryFolder, "test.txt");
-		countDownLatch.await(200, TimeUnit.MILLISECONDS);
+        //when
+        monitor.startWatching();
+        Path testFile = Filesystem.createFile(temporaryFolder, "test.txt");
+        countDownLatch.await(200, TimeUnit.MILLISECONDS);
 
-		//then
-		Assertions.assertThat(receivedEvents).hasSize(1);
-		Assertions.assertThat(receivedEvents).extracting(FilesystemEvent::getPath).containsExactly(testFile);
-	}
+        //then
+        Assertions.assertThat(receivedEvents).hasSize(1);
+        Assertions.assertThat(receivedEvents).extracting(FilesystemEvent::getPath).containsExactly(testFile);
+    }
 
-	@Test
-	void shouldWatchDeletedFile() throws InterruptedException {
-		//given
-		Path testFile = Filesystem.createFile(temporaryFolder, "test.txt");
+    @Test
+    void shouldWatchDeletedFile() throws InterruptedException {
+        //given
+        Path testFile = Filesystem.createFile(temporaryFolder, "test.txt");
 
-		CountDownLatch latch = new CountDownLatch(2);
-		List<FilesystemEvent> receivedEvents = new ArrayList<>();
-		FilesystemMonitor monitor = FilesystemMonitor.builder()
-				.watchedPath(temporaryFolder)
-				.watchedConsumer(event -> {
-					receivedEvents.add(event);
-					latch.countDown();
-				})
-				.build();
+        CountDownLatch latch = new CountDownLatch(2);
+        List<FilesystemEvent> receivedEvents = new ArrayList<>();
+        FilesystemMonitor monitor = FilesystemMonitor.builder()
+                .watchedPath(temporaryFolder)
+                .watchedConsumer(event -> {
+                    receivedEvents.add(event);
+                    latch.countDown();
+                })
+                .build();
 
-		//when
-		monitor.startWatching();
-		Filesystem.deleteFile(testFile);
-		latch.await(2000, TimeUnit.MILLISECONDS);
+        //when
+        monitor.startWatching();
+        Filesystem.deleteFile(testFile);
+        latch.await(2000, TimeUnit.MILLISECONDS);
 
-		//then
-		Assertions.assertThat(receivedEvents).hasSize(2);
-		Assertions.assertThat(receivedEvents).extracting(FilesystemEvent::getPath)
-				.contains(testFile, testFile);
-		Assertions.assertThat(receivedEvents).extracting(FilesystemEvent::getEventType)
-				.contains(INITIAL, DELETED);
+        //then
+        Assertions.assertThat(receivedEvents).hasSize(2);
+        Assertions.assertThat(receivedEvents).extracting(FilesystemEvent::getPath)
+                .contains(testFile, testFile);
+        Assertions.assertThat(receivedEvents).extracting(FilesystemEvent::getEventType)
+                .contains(INITIAL, DELETED);
 
-	}
+    }
 
-	@ParameterizedTest
-	@MethodSource
-	void shouldWatchModifiedFile(PathKind pathKind, ModificationKind strategy) throws InterruptedException {
-		//given
-		Path testFile = pathKind.apply(temporaryFolder);
-		CountDownLatch latch = new CountDownLatch(2);
-		List<FilesystemEvent> receivedEvents = new ArrayList<>();
-		FilesystemMonitor monitor = FilesystemMonitor.builder()
-				.watchedPath(temporaryFolder)
-				.watchedConsumer(event -> {
-					receivedEvents.add(event);
-					latch.countDown();
-				})
-				.build();
-		//when
-		monitor.startWatching();
-		strategy.accept(testFile);
-		latch.await(2000, TimeUnit.MILLISECONDS);
+    @ParameterizedTest
+    @MethodSource
+    void shouldWatchModifiedFile(PathKind pathKind, ModificationKind strategy) throws InterruptedException {
+        //given
+        Path testFile = pathKind.apply(temporaryFolder);
+        CountDownLatch latch = new CountDownLatch(2);
+        List<FilesystemEvent> receivedEvents = new ArrayList<>();
+        FilesystemMonitor monitor = FilesystemMonitor.builder()
+                .watchedPath(temporaryFolder)
+                .watchedConsumer(event -> {
+                    receivedEvents.add(event);
+                    latch.countDown();
+                })
+                .build();
+        //when
+        monitor.startWatching();
+        strategy.accept(testFile);
+        latch.await(2000, TimeUnit.MILLISECONDS);
 
-		//then
-		Assertions.assertThat(receivedEvents).hasSize(1);
-		Assertions.assertThat(receivedEvents).extracting(FilesystemEvent::getPath)
-				.contains(testFile, testFile);
-		Assertions.assertThat(receivedEvents).extracting(FilesystemEvent::getEventType)
-				.contains(MODIFIED);
-	}
+        //then
+        Assertions.assertThat(receivedEvents).hasSize(1);
+        Assertions.assertThat(receivedEvents).extracting(FilesystemEvent::getPath)
+                .contains(testFile, testFile);
+        Assertions.assertThat(receivedEvents).extracting(FilesystemEvent::getEventType)
+                .contains(MODIFIED);
+    }
 
-	public Stream<Arguments> shouldWatchModifiedFile() {
-		Array<ModificationKind> modificationStrategies = Array.of(ModificationKind.values());
-		Array<PathKind> pathKinds = Array.of(PathKind.values());
+    public Stream<Arguments> shouldWatchModifiedFile() {
+        Array<ModificationKind> modificationStrategies = Array.of(ModificationKind.values());
+        Array<PathKind> pathKinds = Array.of(PathKind.values());
 
-		var arguments = pathKinds.crossProduct(modificationStrategies);
-		return arguments.toJavaStream().map(tuple -> Arguments.of(tuple._1(), tuple._2()));
-	}
+        var arguments = pathKinds.crossProduct(modificationStrategies);
+        return arguments.toJavaStream().map(tuple -> Arguments.of(tuple._1(), tuple._2()));
+    }
 }
