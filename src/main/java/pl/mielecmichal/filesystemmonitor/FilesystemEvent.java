0e24609f7b5ee92ad329cb2413bc2b4adc8ab883
mielecmichal <mielecmichal@gmail.com>
2019-07-19 20:14:05 +0200

    Simplify FilesystemEvent creation by better factory method.

--- src/main/java/pl/mielecmichal/filesystemmonitor/FilesystemEvent.java
+++ src/main/java/pl/mielecmichal/filesystemmonitor/FilesystemEvent.java
@@ -1,6 +1,7 @@
 package pl.mielecmichal.filesystemmonitor;
 
-import lombok.Builder;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Value;
 
 import java.nio.file.Path;
@@ -8,24 +9,14 @@ import java.nio.file.Paths;
 import java.nio.file.WatchEvent;
 
 @Value
-@Builder
+@AllArgsConstructor(access = AccessLevel.PACKAGE, staticName = "of")
 public class FilesystemEvent {
     private final Path path;
     private final FilesystemEventType eventType;
 
-    public static FilesystemEvent of(FilesystemEventType eventType, Path path) {
-        return FilesystemEvent.builder()
-                .eventType(eventType)
-                .path(path)
-                .build();
-    }
-
-    static FilesystemEvent of(WatchEvent watchedEvent, Path watchedDirectoryPath) {
-        Path watchedElement = (Path) watchedEvent.context();
-        Path totalPath = Paths.get(watchedDirectoryPath.toString(), watchedElement.toString());
-        return FilesystemEvent.builder()
-                .eventType(FilesystemEventType.of(watchedEvent.kind()))
-                .path(totalPath)
-                .build();
+    static FilesystemEvent of(WatchEvent event, Path path) {
+        Path watchedElement = (Path) event.context();
+        Path totalPath = Paths.get(path.toString(), watchedElement.toString());
+        return new FilesystemEvent(totalPath, FilesystemEventType.of(event.kind()));
     }
 }
--- src/test/java/pl/mielecmichal/filesystemmonitor/RecursivePathTest.java
+++ src/test/java/pl/mielecmichal/filesystemmonitor/RecursivePathTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.params.provider.EnumSource;
 import pl.mielecmichal.filesystemmonitor.parameters.ModificationKind;
 import pl.mielecmichal.filesystemmonitor.utilities.Filesystem;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,43 +21,37 @@ import static pl.mielecmichal.filesystemmonitor.FilesystemEventType.MODIFIED;
 @Slf4j
 class RecursivePathTest {
 
-	@ParameterizedTest
-	@EnumSource(ModificationKind.class)
-	void shouldWatchModifiedFile(ModificationKind modificationKind, @TempDir Path temporaryDirectory) throws InterruptedException {
-		//given
-		Path recursive = Filesystem.createDirectory(temporaryDirectory, "recursive");
-		Path recursiveFile = Filesystem.createFile(recursive, "recursive.txt");
+    @ParameterizedTest
+    @EnumSource(ModificationKind.class)
+    void shouldWatchModifiedFile(ModificationKind modificationKind, @TempDir Path temporaryDirectory) throws InterruptedException {
+        //given
+        Path recursive = Filesystem.createDirectory(temporaryDirectory, "recursive");
+        Path recursiveFile = Filesystem.createFile(recursive, "recursive.txt");
 
-		//when
-		CountDownLatch countDownLatch = new CountDownLatch(3);
-		List<FilesystemEvent> receivedEvents = new ArrayList<>();
-		FilesystemNotifier monitor = FilesystemMonitor.builder()
-				.watchedPath(temporaryDirectory)
-				.watchedConstraints(FilesystemConstraints.DEFAULT.withRecursive(true))
-				.watchedConsumer(event -> {
-					try {
-						log.info("permissions: {}", Files.getPosixFilePermissions(event.getPath()));
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+        //when
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+        List<FilesystemEvent> receivedEvents = new ArrayList<>();
+        FilesystemNotifier monitor = FilesystemMonitor.builder()
+                .watchedPath(temporaryDirectory)
+                .watchedConstraints(FilesystemConstraints.DEFAULT.withRecursive(true))
+                .watchedConsumer(event -> {
+                    receivedEvents.add(event);
+                    countDownLatch.countDown();
+                })
+                .build();
 
-					receivedEvents.add(event);
-					countDownLatch.countDown();
-				})
-				.build();
+        //when
+        monitor.startWatching();
+        Thread.sleep(100);
+        modificationKind.accept(recursiveFile);
 
-		//when
-		monitor.startWatching();
-		Thread.sleep(100);
-		modificationKind.accept(recursiveFile);
+        countDownLatch.await(TEST_TIMEOUT.toMillis(), TimeUnit.MILLISECONDS);
 
-		countDownLatch.await(TEST_TIMEOUT.toMillis(), TimeUnit.MILLISECONDS);
-
-		//then
-		Assertions.assertThat(receivedEvents).contains(
-				FilesystemEvent.builder().path(recursive).eventType(INITIAL).build(),
-				FilesystemEvent.builder().path(recursiveFile).eventType(INITIAL).build(),
-				FilesystemEvent.builder().path(recursiveFile).eventType(MODIFIED).build()
-		);
-	}
+        //then
+        Assertions.assertThat(receivedEvents).contains(
+                FilesystemEvent.of(recursive, INITIAL),
+                FilesystemEvent.of(recursiveFile, INITIAL),
+                FilesystemEvent.of(recursiveFile, MODIFIED)
+        );
+    }
 }
