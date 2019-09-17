d2596e693182ee699cf44f1526e66171f05c9546
Michał Mielec <mielecmichal@gmail.com>
2019-09-18 00:36:01 +0200

    Remove thread sleep from tests.

--- src/test/java/pl/mielecmichal/filesystemmonitor/utilities/AwaitilityUtils.java
+++ src/test/java/pl/mielecmichal/filesystemmonitor/utilities/AwaitilityUtils.java
@@ -7,9 +7,11 @@ import java.util.Collection;
 
 public class AwaitilityUtils {
 
-	private static final Duration DEFAULT_DURATION = Duration.ONE_HUNDRED_MILLISECONDS;
+	private static final Duration DEFAULT_WAIT_TIME = Duration.ONE_HUNDRED_MILLISECONDS;
 
 	public static void awaitForSize(Collection collection, int expectedSize) {
-		Awaitility.await().atLeast(DEFAULT_DURATION).until(() -> collection.size() == expectedSize);
+		Awaitility.await()
+				.atLeast(DEFAULT_WAIT_TIME)
+				.until(() -> collection.size() == expectedSize);
 	}
 }
