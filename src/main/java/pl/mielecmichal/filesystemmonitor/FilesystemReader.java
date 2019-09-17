aeb2e243d05e17b4d2f3bdf181b01cc9819893fb
Michał Mielec <mielecmichal@gmail.com>
2019-09-17 14:38:53 +0200

    Fix skipping recursive directories by reader correctly.

--- src/main/java/pl/mielecmichal/filesystemmonitor/FilesystemReader.java
+++ src/main/java/pl/mielecmichal/filesystemmonitor/FilesystemReader.java
@@ -1,8 +1,6 @@
 package pl.mielecmichal.filesystemmonitor;
 
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Value;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -23,65 +21,67 @@ import static pl.mielecmichal.filesystemmonitor.FilesystemEventType.INITIAL;
 @Value
 public class FilesystemReader implements FilesystemNotifier {
 
-    private final Path watchedPath;
-    private final FilesystemConstraints watchedConstraints;
-    private final Consumer<FilesystemEvent> watchedConsumer;
+	private final Path watchedPath;
+	private final FilesystemConstraints watchedConstraints;
+	private final Consumer<FilesystemEvent> watchedConsumer;
 
-    @Override
-    public void startWatching() {
-        ConstraintsFilteringVisitor visitor = new ConstraintsFilteringVisitor(watchedPath, watchedConstraints);
-        try {
-            Files.walkFileTree(watchedPath, visitor);
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
+	@Override
+	public void startWatching() {
+		ConstraintsFilteringVisitor visitor = new ConstraintsFilteringVisitor(watchedPath, watchedConstraints);
+		try {
+			Files.walkFileTree(watchedPath, visitor);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
 
-        visitor.getEvents().forEach(watchedConsumer);
-    }
+		visitor.getEvents().forEach(watchedConsumer);
+	}
 
-    @Override
-    public void stopWatching() {
-        // For now we do not need to implement this. Maybe later to make this class more responsive to stop.
-    }
+	@Override
+	public void stopWatching() {
+		// For now we do not need to implement this. Maybe later to make this class more responsive to stop.
+	}
 
-    @Value
-    @EqualsAndHashCode(callSuper = true)
-    private static final class ConstraintsFilteringVisitor extends SimpleFileVisitor<Path> {
+	@RequiredArgsConstructor
+	@EqualsAndHashCode(callSuper = true)
+	private static final class ConstraintsFilteringVisitor extends SimpleFileVisitor<Path> {
 
-        private final Path watchedPatch;
-        private final List<FilesystemEvent> events = new ArrayList<>();
-        private final FilesystemConstraints constraints;
+		private final Path watchedPatch;
+		private final FilesystemConstraints constraints;
+		@Getter
+		private final List<FilesystemEvent> events = new ArrayList<>();
 
-        @Override
-        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-            super.postVisitDirectory(dir, exc);
+		@Override
+		public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+			super.preVisitDirectory(dir, attrs);
 
-            if (dir.equals(watchedPatch)) {
-                return FileVisitResult.CONTINUE;
-            }
+			if (dir.equals(watchedPatch)) {
+				return FileVisitResult.CONTINUE;
+			}
 
-            addFilesystemEvent(dir);
+			addFilesystemEvent(dir);
 
-            if (!constraints.isRecursive()) {
-                return FileVisitResult.CONTINUE;
-            }
+			if (!constraints.isRecursive()) {
+				return FileVisitResult.SKIP_SUBTREE;
+			}
 
-            return FileVisitResult.CONTINUE;
-        }
+			return FileVisitResult.CONTINUE;
+		}
 
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-            super.visitFile(file, attrs);
-            addFilesystemEvent(file);
-            return FileVisitResult.CONTINUE;
-        }
+		@Override
+		public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+			super.visitFile(file, attrs);
+			addFilesystemEvent(file);
+			return FileVisitResult.CONTINUE;
+		}
 
-        private void addFilesystemEvent(Path path) {
-            FilesystemEvent filesystemEvent = FilesystemEvent.of(path, INITIAL);
+		private void addFilesystemEvent(Path path) {
+			FilesystemEvent filesystemEvent = FilesystemEvent.of(path, INITIAL);
 
-            if (constraints.test(filesystemEvent)) {
-                events.add(filesystemEvent);
-            }
-        }
-    }
+			if (constraints.test(filesystemEvent)) {
+				events.add(filesystemEvent);
+				log.info("Created event: {}", filesystemEvent);
+			}
+		}
+	}
 }
