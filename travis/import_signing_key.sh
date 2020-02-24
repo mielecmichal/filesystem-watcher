3222ac18d80dcd9ef3f539edf76bb35f414ae2a4
Michał Mielec <mielecmichal@gmail.com>
2020-02-24 22:28:19 +0100

    Introduce windows tests

--- travis/import_signing_key.sh
+++ travis/import_signing_key.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-if [ "${TRAVIS_OS_NAME}" != "osx" ]; then
 openssl aes-256-cbc -K $encrypted_92bb71d8d05e_key -iv $encrypted_92bb71d8d05e_iv -in travis/signing_key.gpg.enc -out signing_key.gpg -d
 gpg2 --batch --import signing_key.gpg
-fi
