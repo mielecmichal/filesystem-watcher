3f51c10cb7b7cd554bee5937e913d6303c184072
Michał Mielec <mielecmichal@gmail.com>
2020-02-24 22:48:36 +0100

    Introduce windows tests

--- travis/install_windows_java.bat
+++ travis/install_windows_java.bat
@@ -1,3 +1,4 @@
+
+choco install openjdk11 -params 'installdir=c:\\jdk' -y
 export JAVA_HOME=${JAVA_HOME:-/c/jdk}
 export PATH=${JAVA_HOME}/bin:${PATH}
-choco install openjdk11 -params 'installdir=c:\\jdk' -y
