a179456c683da795748ecb960c1e3fd2e2f3db97
Michał Mielec <mielecmichal@gmail.com>
2020-02-26 17:51:48 +0100

    Increase acceptable observation time to 2 highest sensitivities available.

--- src/test/java/io/github/filesystemwatcher/utilities/AwaitilityUtils.java
+++ src/test/java/io/github/filesystemwatcher/utilities/AwaitilityUtils.java
@@ -1,5 +1,6 @@
 package io.github.filesystemwatcher.utilities;
 
+import com.sun.nio.file.SensitivityWatchEventModifier;
 import lombok.experimental.UtilityClass;
 import org.awaitility.Awaitility;
 
@@ -10,17 +11,17 @@ import java.util.Map;
 @UtilityClass
 public class AwaitilityUtils {
 
-    private static final Duration DEFAULT_WAIT_TIME = Duration.ofSeconds(1);
+    private static final Duration DEFAULT_WAIT_TIME = Duration.ofSeconds(2 * SensitivityWatchEventModifier.HIGH.sensitivityValueInSeconds());
 
-	public static void awaitForSize(Collection collection, int expectedSize) {
-		Awaitility.await()
-				.atMost(DEFAULT_WAIT_TIME)
-				.until(() -> collection.size() == expectedSize);
-	}
+    public static <T> void awaitForSize(Collection<T> collection, int expectedSize) {
+        Awaitility.await()
+                .atMost(DEFAULT_WAIT_TIME)
+                .until(() -> collection.size() == expectedSize);
+    }
 
-	public static void awaitForSize(Map map, int expectedSize) {
-		Awaitility.await()
-				.atMost(DEFAULT_WAIT_TIME)
-				.until(() -> map.size() == expectedSize);
-	}
+    public static <K, V> void awaitForSize(Map<K, V> map, int expectedSize) {
+        Awaitility.await()
+                .atMost(DEFAULT_WAIT_TIME)
+                .until(() -> map.size() == expectedSize);
+    }
 }
